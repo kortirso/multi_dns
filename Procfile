ee96debe099b7ac66038ebfcdf42003246a0a10e
Bogdanov Anton <kortirso@gmail.com>
2023-10-08 13:15:49 +0300

    remove js

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: bin/rails server -p 5000
-js: yarn build --watch
 css: bin/rails tailwindcss:watch
--- config/environments/local_production.rb
+++ config/environments/local_production.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
